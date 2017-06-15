42f11faa3dd4b8ad3d379e137bdda9e5f89518dc
Ala Otto <alicja.otto@gmail.com>
2017-06-15 17:48:08 +0200

    content ready

--- app.coffee
+++ app.coffee
@@ -2,9 +2,10 @@ axis         = require 'axis'
 rupture      = require 'rupture'
 autoprefixer = require 'autoprefixer-stylus'
 js_pipeline  = require 'js-pipeline'
-#css_pipeline = require 'css-pipeline'
+css_pipeline = require 'css-pipeline'
 handlebars   = require 'handlebars'
-sass = require 'roots-sass'
+# sass         = require 'roots-sass'
+
 
 
 module.exports =
@@ -12,9 +13,8 @@ module.exports =
 
   extensions: [
     js_pipeline(files: 'assets/js/*.coffee'),
-    #css_pipeline(files: 'assets/css/*.styl'),
-    sass(files: "assets/css/main.sass", out: 'css/style.css', style: 'compressed')
-
+    css_pipeline(files: 'assets/css/*.styl')
+    # sass(files: "assets/css/main.scss", out: 'css/style.css', style: 'compressed')
   ]
 
   stylus:
@@ -24,5 +24,5 @@ module.exports =
   'coffee-script':
     sourcemap: true
 
-  handlebars:
+  jade:
       pretty: true
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,32 +2,75 @@ $(document).ready(function() {
 
     console.log('hello world!')
 
-    var data = {
-        title: "Beside the Park",
-        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+//header scripts and data
+    const headerTemplate = $('#header-template').html();
+    const compiledHeaderTemplate = Handlebars.compile(headerTemplate);
+
+    const header_data = {
+        logo: "Beside the Park",
+        introduction_title: "Lorem ipsum",
+        introduction_text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        menu_png: "img/menu.png",
+        search_png: "img/search.png",
+        links: [
+            {menu_link: "#about", text: "about"},
+            {menu_link: "#portfolio", text: "portfolio"},
+            {menu_link: "#contact", text: "contact us"}
+        ],
+
+
     };
 
-    var template = $('#header').html();
-    console.log(template);
+    const headerHTML = compiledHeaderTemplate(header_data);
 
+    $('header').append(headerHTML);
 
-    var compiledTemplate = Handlebars.compile(template);
-    console.log(compiledTemplate);
+//sction about scripts and data
+    const aboutTemplate = $('#about-template').html();
+    const compiledAboutTemplate = Handlebars.compile(aboutTemplate);
 
-    // $('#page').html(compiledTemplate(data));
-
-    var html = compiledTemplate(data);
-    console.log(html)
-
-    $('#page').append(html);
-
-    // $('#page').append(Handlebars.templates.index(data));
-
-    var CreateConsole = () => {
-        console.log("ES6!");
-
+    const about_data = {
+        title: "Lorem ipsum",
+        paragraph1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        paragraph2: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.",
+        paragraph3: "eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+        paragraph4: "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
+        paragraph5: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur."
     }
 
-    CreateConsole();
+    const aboutHTML = compiledAboutTemplate(about_data);
+
+    $('#about').append(aboutHTML);
+
+//sction offer scripts and data
+    const offerTemplate = $('#offer-template').html();
+    const compiledOfferTemplate = Handlebars.compile(offerTemplate);
+
+    const offer_data = {
+        offer1_title: "Built to last",
+        offer1_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        offer1_link: "http://findmore.org",
+        offer1_link_title: "Find out more",
+
+        offer2_title: "Our clients",
+        offer2_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+        offer2_link: "'http://findmore.org'",
+        offer2_link_title: "Get insights",
+
+        offer3_title: "Sign up & see why",
+        offer3_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        offer3_link: "http://findmore.org",
+        offer3_link_title: "Get started now",
+
+        offer4_title: "Perfect pictures",
+        offer4_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+        offer4_link: "http://findmore.org",
+        offer4_link_title: "Find out more"
+    }
+
+    const offerHTML = compiledOfferTemplate(offer_data);
+
+    $('#offer').append(offerHTML);
+
 
 });
