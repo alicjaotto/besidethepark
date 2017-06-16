590417ae87c6f1acceffc678ed62a4ec5e965e75
Ala Otto <alicja.otto@gmail.com>
2017-06-16 14:08:45 +0200

    project compiled. es6 to es5 compiler installed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
 $(document).ready(function () {
 
     console.log('hello world!'
 
     //header scripts and data
-    );const headerTemplate = $('#header-template').html();
-    const compiledHeaderTemplate = Handlebars.compile(headerTemplate);
+    );var headerTemplate = $('#header-template').html();
+    var compiledHeaderTemplate = Handlebars.compile(headerTemplate);
 
-    const header_data = {
+    var header_data = {
         logo: "Beside the Park",
         hyperlink: "http://besidethepark.com",
         introduction_title: "Lorem ipsum",
@@ -17,15 +20,15 @@ $(document).ready(function () {
 
     };
 
-    const headerHTML = compiledHeaderTemplate(header_data);
+    var headerHTML = compiledHeaderTemplate(header_data);
 
     $('header').append(headerHTML);
 
     //sction about scripts and data
-    const aboutTemplate = $('#about-template').html();
-    const compiledAboutTemplate = Handlebars.compile(aboutTemplate);
+    var aboutTemplate = $('#about-template').html();
+    var compiledAboutTemplate = Handlebars.compile(aboutTemplate);
 
-    const about_data = {
+    var about_data = {
         title: "Lorem ipsum",
         paragraph1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
         paragraph2: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.",
@@ -34,15 +37,15 @@ $(document).ready(function () {
         paragraph5: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur."
     };
 
-    const aboutHTML = compiledAboutTemplate(about_data);
+    var aboutHTML = compiledAboutTemplate(about_data);
 
     $('#about').append(aboutHTML);
 
     //sction offer scripts and data
-    const offerTemplate = $('#offer-template').html();
-    const compiledOfferTemplate = Handlebars.compile(offerTemplate);
+    var offerTemplate = $('#offer-template').html();
+    var compiledOfferTemplate = Handlebars.compile(offerTemplate);
 
-    const offer_data = {
+    var offer_data = {
         offer1_title: "Built to last",
         offer1_text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         offer1_link: "http://findmore.org",
@@ -64,17 +67,20 @@ $(document).ready(function () {
         offer4_link_title: "Find out more"
     };
 
-    const offerHTML = compiledOfferTemplate(offer_data);
+    var offerHTML = compiledOfferTemplate(offer_data);
 
     $('#offer').append(offerHTML);
 
     //menu and search visibility
 
-    $('#menu_button').on("click", event => {
+    $('#menu_button').on("click", function (event) {
         $('#menu').toggleClass('hidden');
     });
 
-    $('#search_button').on('click', event => {
+    $('#search_button').on('click', function (event) {
         $('#search_input').toggleClass('hidden');
     });
 });
+
+},{}]},{},[1])
+//# sourceMappingURL=main.js.map
